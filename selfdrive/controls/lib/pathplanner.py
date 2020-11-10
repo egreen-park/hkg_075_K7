15f9858a39fc6c71951e1ae27448489735467a1e
alexhys74 <alexhys74@gmail.com>
2020-11-10 15:27:34 +0900

    수정

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -14,7 +14,7 @@ LaneChangeState = log.PathPlan.LaneChangeState
 LaneChangeDirection = log.PathPlan.LaneChangeDirection
 LaneChangeBSM = log.PathPlan.LaneChangeBSM
 
-LOG_MPC = True #os.environ.get('LOG_MPC', False)
+LOG_MPC = os.environ.get('LOG_MPC', False) #Ture
 
 LANE_CHANGE_SPEED_MIN = 45 * CV.KPH_TO_MS
 LANE_CHANGE_TIME_MAX = 10.
@@ -121,11 +121,11 @@ class PathPlanner():
 
       if self.lane_change_direction == LaneChangeDirection.left:
         torque_applied = sm['carState'].steeringTorque > 0 and sm['carState'].steeringPressed
-        if CP.autoLcaEnabled and 2.5 > self.pre_auto_LCA_timer > 2.0 and not lca_left:
+        if CP.autoLcaEnabled and 0.5 > self.pre_auto_LCA_timer > 0.2 and not lca_left:
           torque_applied = True # Enable auto LCA only once after 2 sec 
       else:
         torque_applied = sm['carState'].steeringTorque < 0 and sm['carState'].steeringPressed
-        if CP.autoLcaEnabled and 2.5 > self.pre_auto_LCA_timer > 2.0 and not lca_right:
+        if CP.autoLcaEnabled and 0.5 > self.pre_auto_LCA_timer > 0.2 and not lca_right:
           torque_applied = True # Enable auto LCA only once after 2 sec 
 
       lane_change_prob = self.LP.l_lane_change_prob + self.LP.r_lane_change_prob
